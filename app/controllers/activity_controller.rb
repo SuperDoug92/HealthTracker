099051a58b4585642663691be811a8134e00e217
Doug <clarke.doug248@gmail.com>
2015-11-21 16:45:24 -0500

    activity controller create method

--- app/controllers/activity_controller.rb
+++ app/controllers/activity_controller.rb
@@ -1,6 +1,31 @@
 class ActivityController < ApplicationController
 
-  def new
+  def create
+    @user = User.find(params[:user_id])
+    @activities = @user.activities
+
+    @activity = current_user.activities.build(activity_params)
+    @activity.user = @user
+    @new_activity = activity.new
+
+    authorize @activity
+
+    if @activity.save
+      flash[:notice] = "activity was created."
+    else
+      flash[:error] = "There was an error saving the activity. Please try again."
+    end
+
+    respond_to do |format|
+      format.html
+      format.js
+    end
   end
-  
+
+  private
+
+  def activity_params
+    params.require(:activity).permit(:body)
+  end
+
 end
